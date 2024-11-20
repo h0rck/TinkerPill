9f796fff5aff4d7f1fb4cc1293fe5d58d9d0e1a3
Sam <s4muelrp@gmail.com>
2024-11-19 21:47:04 -0300

    add Electron main process and preload script; integrate tinker command execution

---
+++ electron/Events/tinkerCommandEvent.ts
@@ -0,0 +1,44 @@
+import { ipcMain } from 'electron';
+import { spawn } from 'child_process';
+
+
+const containerName = 'devlocalhost.php-api';
+
+export function tinkerCommandEvent() {
+    
+    ipcMain.handle('execute-tinker-command', async (_event, command) => {
+        return new Promise((resolve) => {
+          command = command.replace(/^\s*<\?php\s*/, '').trim();
+    
+          const phpCommand = `
+            DB::enableQueryLog();
+            $result = ${command};
+            $queries = DB::getQueryLog();
+            echo json_encode(['result' => $result, 'queries' => $queries], JSON_PRETTY_PRINT);
+          `;
+    
+          const tinkerProcess = spawn('docker', [
+            'exec',
+            '-i',
+            containerName,
+            'php',
+            'artisan',
+            'tinker',
+            '--env=nilopoliseduca.dev'
+          ]);
+    
+          let output = '';
+    
+          tinkerProcess.stdout.on('data', (data) => {
+            output += data.toString();
+          });
+    
+          tinkerProcess.on('close', () => {
+            resolve(output);
+          });
+    
+          tinkerProcess.stdin.write(`${phpCommand}\n`);
+          tinkerProcess.stdin.end();
+        });
+      });
+    }
